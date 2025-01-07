eb14009c2a0c0996b9ceca8e956e42bb756b8a95
FelipeMarra <felipeferreiramarra@gmail.com>
2025-01-07 15:23:04 -0300

    vectorized multihead self att

--- src/vivit/att_blocks.py
+++ src/vivit/att_blocks.py
@@ -1,41 +1,63 @@
+from typing import Tuple
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-class AttHead(nn.Module):
-    def __init__(self, emd_dim, head_size):
-        super().__init__()
-        self.wq = nn.Linear(emd_dim, head_size, bias=False)
-        self.wk = nn.Linear(emd_dim, head_size, bias=False)
-        self.wv = nn.Linear(emd_dim, head_size, bias=False)
-        self.dropout = nn.Dropout(0.1)
+def attention(query:torch.Tensor, key:torch.Tensor, value:torch.Tensor, dropout:nn.Module) -> Tuple[torch.Tensor, torch.Tensor]:
+    head_size = query.size(-1)
 
-    def forward(self, x:torch.Tensor) -> torch.Tensor:
-        _, _, E = x.shape # (B, S, E)
+    # Words similarity q (B, n_heads, S, head_size) @ k (B, n_heads, head_size, S) = x (B, n_heads, S, S)
+    att = query @ key.transpose(-2, -1)
+    # Scale by sqrt(head_size)
+    att = att * head_size**-0.5
+    # Final attention weights (B, n_heads, S, S)
+    att = F.softmax(att, dim=-1)
+    att = dropout(att)
 
-        q:torch.Tensor = self.wq(x) # x (B, S, E) @ wq (E, E) = q (B, S, E)
-        k:torch.Tensor = self.wk(x) # x (B, S, E) @ wk (E, E) = k (B, S, E)
-        v:torch.Tensor = self.wv(x) # x (B, S, E) @ wv (E, E) = v (B, S, E)
+    # Weighted average att (B, n_heads, S, S) @ v (B, n_heads, S, head_size) = x (B, n_heads, S, head_size)
+    x = att @ value
 
-        att = q @ k.transpose(-2, -1) # Words similarity q (B, S, E) @ k (B, E, S) = x (B, S, S)
-        att:torch.Tensor = att * E**-0.5 # scale
-        att = F.softmax(att, dim=-1)  # Final attention weights (B, S, S)
-        att = self.dropout(att)
-
-        x = att @ v # Weighted average x (B, S, S) @ v (B, S, E) = x (B, S, E)
-
-        return x
+    return x, att
 
 class MultiHeadAtt(nn.Module):
-    def __init__(self, emb_dim, head_size, n_heads):
+    def __init__(self, emb_dim, n_heads):
         super().__init__()
 
-        self.heads = nn.ModuleList([AttHead(emb_dim, head_size) for _ in range(n_heads)])
+        assert emb_dim % n_heads == 0
+        self.head_size = emb_dim // n_heads
+        self.n_heads = n_heads
+
+        self.wq = nn.Linear(emb_dim, emb_dim, bias=False)
+        self.wk = nn.Linear(emb_dim, emb_dim, bias=False)
+        self.wv = nn.Linear(emb_dim, emb_dim, bias=False)
+        self.wo = nn.Linear(emb_dim, emb_dim, bias=False)
+
+        self.att = None
         self.proj = nn.Linear(emb_dim, emb_dim) # projection to the residual connetion
         self.dropout = nn.Dropout(0.1)
 
-    def forward(self, x):
-        # cat in the E dimention. if head size is E/2 and n_heads is 2, out will still be (B, S, E) 
-        x = torch.cat([head(x) for head in self.heads], dim=-1)
-        x = self.proj(x)
-        return self.dropout(x)
+    def forward(self, x:torch.Tensor):
+        B, S, E = x.shape
+
+        query:torch.Tensor = self.wq(x)
+        key:torch.Tensor = self.wk(x)
+        value:torch.Tensor = self.wv(x)
+
+        # q, k and v will be (B, n_heads, S, head_size)
+        query = query.view(B, S, self.n_heads, self.head_size).transpose(1,2)
+        key = key.view(B, S, self.n_heads, self.head_size).transpose(1,2)
+        value = value.view(B, S, self.n_heads, self.head_size).transpose(1,2)
+
+        # x is (B, n_heads, S, head_size)
+        # att is (B, n_heads, S, S)
+        x, self.att = attention(query, key, value, self.dropout)
+
+        # transpose to (B, S, n_heads, head_size) 
+        # and go back to (B, S, E)
+        x = x.transpose(1, 2).contiguous().view(B, S, E)
+
+        del query
+        del key
+        del value
+
+        return self.wo(x)
--- src/vivit/encoder_block.py
+++ src/vivit/encoder_block.py
@@ -4,12 +4,13 @@ from vivit.att_blocks import MultiHeadAtt
 class FeedForward(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
-        self.linear = nn.Linear(emb_dim, 4 * emb_dim)
+        self.linear = nn.Linear(emb_dim, 4 * emb_dim) # they use 3072 wich is 4 * 768 (they embed dim)
         self.proj = nn.Linear(4 * emb_dim, emb_dim) # projection to the residual connetion
         self.dropout = nn.Dropout(0.1)
 
     def forward(self, x):
         x = nn.GELU()(self.linear(x))
+        # TODO: Dropout? https://github.com/google-research/scenic/blob/97d6ac5b65040621f266b0da3bf05066baa664f3/scenic/model_lib/layers/attention_layers.py#L399
         x = self.proj(x)
         return self.dropout(x)
 
@@ -17,11 +18,8 @@ class EncoderBlock(nn.Module):
     def __init__(self, emb_dim, n_heads):
         super().__init__()
 
-        assert emb_dim % n_heads == 0
-        head_size = emb_dim // n_heads
-
         self.norm1 = nn.LayerNorm(emb_dim)
-        self.multi_head_att = MultiHeadAtt(emb_dim, head_size, n_heads)
+        self.multi_head_att = MultiHeadAtt(emb_dim, n_heads)
         self.dropout = nn.Dropout(0.1)
         self.norm2 = nn.LayerNorm(emb_dim)
         self.ff = FeedForward(emb_dim)
