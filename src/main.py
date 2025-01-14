d03a3a75808decdbb49247603198edc4214331cb
FelipeMarra <felipeferreiramarra@gmail.com>
2025-01-14 17:24:36 -0300

    added multi-GPU with DDP

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 #%% 
 # Imports
+import os
 from datetime import datetime
 import random
 import multiprocessing as mp
 import torch
 import numpy as np
-from consts import *
+import consts as c
 from datasets.datasets import get_datasets
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2
@@ -13,82 +14,100 @@ from vivit.vivit import ViViT
 from train_utils import train, test
 from torch.utils.tensorboard import SummaryWriter
 
-#%% 
-# Parameters asserts
-assert EMB_DIM % N_HEADS == 0, "EMB_DIM should be divisible by N_HEAD"
-assert N_PATCHES.is_integer(), "N_PATCHES should be an integer"
-N_PATCHES = int(N_PATCHES)
+import torch.multiprocessing as torch_mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.distributed import init_process_group, destroy_process_group
 
-#%%
-# Set seed
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
-torch.cuda.manual_seed_all(SEED)
+def main(rank:int, world_size:int):
+    #%% 
+    # Parameters asserts
+    assert c.EMB_DIM % c.N_HEADS == 0, "EMB_DIM should be divisible by N_HEAD"
+    assert c.N_PATCHES.is_integer(), "N_PATCHES should be an integer"
+    N_PATCHES = int(c.N_PATCHES)
 
-#%% 
-# Loaders
-# Transforms will occur as [T, C, H, W], before chuncks are transposed to [C, T, H, W]
-# RandomResizedCrop was used instead of scale jitter + random crop. DMVR scale jitter implementation is different from the torch one 
-# RandomResizedCrop value from  https://github.com/KSonPham/ViVit-a-Pytorch-implementation/blob/14aaab46a6301a1a786a6372f686d75b6ae7fac5/utils/data_utils.py#L54
-# RandomHorizontalFlip and RandomApply (for color jitter) values are from table 7 in the ViViT paper
-# ColorJitter values are same as DMVR default values: https://github.com/google-deepmind/dmvr/blob/77ccedaa084d29239eaeafddb0b2e83843b613a1/dmvr/processors.py#L602
-train_transforms = v2.Compose([
-        v2.RandomResizedCrop((CROP_SIZE, CROP_SIZE), (0.05, 1)),
-        v2.RandomHorizontalFlip(0.5),
-        v2.RandomApply([
-            v2.ColorJitter(brightness=0.125, contrast=(0.6, 1.4), saturation=(0.6, 1.4), hue=0.2)
-        ], 
-        0.8),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    #%%
+    # Set seed
+    random.seed(c.SEED)
+    np.random.seed(c.SEED)
+    torch.manual_seed(c.SEED)
+    torch.cuda.manual_seed_all(c.SEED)
 
-test_transforms = v2.Compose([
-        v2.Resize((CROP_SIZE, CROP_SIZE)),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    #%%
+    # Multi-GPU stuff
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+    torch.cuda.set_device(rank)
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
 
-train_dataset, eval_dataset, test_dataset = get_datasets(DATASET, DATASET_PATH, N_FRAMES, train_transforms, test_transforms)
+    #%% 
+    # Loaders
+    # Transforms will occur as [T, C, H, W], before chuncks are transposed to [C, T, H, W]
+    # RandomResizedCrop was used instead of scale jitter + random crop. DMVR scale jitter implementation is different from the torch one 
+    # RandomResizedCrop value from  https://github.com/KSonPham/ViVit-a-Pytorch-implementation/blob/14aaab46a6301a1a786a6372f686d75b6ae7fac5/utils/data_utils.py#L54
+    # RandomHorizontalFlip and RandomApply (for color jitter) values are from table 7 in the ViViT paper
+    # ColorJitter values are same as DMVR default values: https://github.com/google-deepmind/dmvr/blob/77ccedaa084d29239eaeafddb0b2e83843b613a1/dmvr/processors.py#L602
+    train_transforms = v2.Compose([
+            v2.RandomResizedCrop((c.CROP_SIZE, c.CROP_SIZE), (0.05, 1)),
+            v2.RandomHorizontalFlip(0.5),
+            v2.RandomApply([
+                v2.ColorJitter(brightness=0.125, contrast=(0.6, 1.4), saturation=(0.6, 1.4), hue=0.2)
+            ], 
+            0.8),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
 
-n_workers = mp.cpu_count() # n_workers == num of threads
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=n_workers)
-val_loader = DataLoader(eval_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=n_workers)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=n_workers)
+    test_transforms = v2.Compose([
+            v2.Resize((c.CROP_SIZE, c.CROP_SIZE)),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
 
-#%%
-# Tensorboard writer
-now = datetime.now().strftime('%m/%d/%Y_%H:%M:%S')
-writer = SummaryWriter(f'vivit_{now}')
+    train_dataset, eval_dataset, test_dataset = get_datasets(c.DATASET, c.DATASET_PATH, c.N_FRAMES, train_transforms, test_transforms)
 
-#%% 
-# Model
-model = ViViT(N_CLASSES, N_PATCHES, TUBLET_SIZE, EMB_DIM, N_HEADS, N_BLOCKS)
+    n_workers = mp.cpu_count() # n_workers == num of threads
+    train_loader = DataLoader(train_dataset, batch_size=c.BATCH_SIZE, shuffle=False, num_workers=n_workers, sampler=DistributedSampler(train_dataset, shuffle=True))
+    val_loader = DataLoader(eval_dataset, batch_size=c.BATCH_SIZE, shuffle=False, num_workers=n_workers, sampler=DistributedSampler(eval_dataset, shuffle=False))
+    test_loader = DataLoader(test_dataset, batch_size=c.BATCH_SIZE, shuffle=False, num_workers=n_workers, sampler=DistributedSampler(test_dataset, shuffle=False))
 
-total_params = sum(p.numel() for p in model.parameters())
-print(f"Number of parameters: {total_params}")
+    #%%
+    # Tensorboard writer
+    now = datetime.now().strftime('%m/%d/%Y_%H:%M:%S')
+    writer = SummaryWriter(f'vivit_{now}')
 
-# TODO not working: writer.add_graph(model.cuda(), next(iter(train_loader))['video'].cuda())
+    #%% 
+    # Model
+    model = ViViT(c.N_CLASSES, N_PATCHES, c.TUBLET_SIZE, c.EMB_DIM, c.N_HEADS, c.N_BLOCKS)
 
-#%% 
-# Train
-train(model, writer, train_loader, val_loader, epochs=EPOCHS, lr=LR, eval_every=EVAL_EVERY)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Number of parameters: {total_params}")
 
-#%%
-# Test
-#model = ViViT(N_CLASSES, N_PATCHES, TUBLET_SIZE, EMB_DIM, N_HEADS, N_BLOCKS)
-#model = model
-# dev = torch.cuda.current_device()
-# checkpoint = torch.load("filename",
-#                         map_location = lambda storage, loc: storage.cuda(dev))
+    # TODO not working: writer.add_graph(model.cuda(), next(iter(train_loader))['video'].cuda())
 
-# If resuming train needs to load scaler
-# https://pytorch.org/tutorials/recipes/recipes/amp_recipe.html#saving-resuming
-# model.load_state_dict(checkpoint["model"])
-# criterion.load_state_dict(checkpoint["optimizer"])
-# scaler.load_state_dict(checkpoint["scaler"])
+    #%% 
+    # Train
+    train(model, writer, train_loader, val_loader, c.EPOCHS, rank, lr=c.LR, eval_every=c.EVAL_EVERY)
 
-loss, acc = test(model, test_loader, writer)
-writer.close()
-print(f"Test Loss {loss}, Acc {acc}")
+    #%%
+    # Test
+    #model = ViViT(N_CLASSES, N_PATCHES, TUBLET_SIZE, EMB_DIM, N_HEADS, N_BLOCKS)
+    #model = model
+    # dev = torch.cuda.current_device()
+    # checkpoint = torch.load("filename",
+    #                         map_location = lambda storage, loc: storage.cuda(dev))
+
+    # If resuming train needs to load scaler
+    # https://pytorch.org/tutorials/recipes/recipes/amp_recipe.html#saving-resuming
+    # model.load_state_dict(checkpoint["model"])
+    # criterion.load_state_dict(checkpoint["optimizer"])
+    # scaler.load_state_dict(checkpoint["scaler"])
+
+    loss, acc = test(model, test_loader, writer, rank)
+    writer.close()
+    print(f"Test Loss {loss}, Acc {acc}")
+
+    destroy_process_group()
+
+if __name__ == "__main__":
+    world_size = torch.cuda.device_count()
+    torch_mp.spawn(main, args=(world_size,), nprocs=world_size)
--- src/train_utils.py
+++ src/train_utils.py
@@ -4,14 +4,16 @@ from tqdm import tqdm
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+from torch.nn.parallel import DistributedDataParallel as DDP
 
-def train(model:nn.Module, writer:SummaryWriter, train_loader:DataLoader, val_loader:DataLoader, epochs:int, lr=1e-3, eval_every=100):
+def train(model:nn.Module, writer:SummaryWriter, train_loader:DataLoader, val_loader:DataLoader, epochs:int, gpu_id:int, lr=1e-3, eval_every=100):
     model = model.train()
-    model = model.cuda()
+    model = model.to(gpu_id)
+    model = DDP(model, device_ids=[gpu_id])
 
     # create a PyTorch optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-2)
-    criterion = nn.CrossEntropyLoss().cuda()
+    criterion = nn.CrossEntropyLoss()
     scaler = torch.amp.GradScaler()
 
     global_step = 0
@@ -19,10 +21,12 @@ def train(model:nn.Module, writer:SummaryWriter, train_loader:DataLoader, val_lo
     mean_train_loss = 0
     mean_eval_loss = 0
 
-    for _ in range(epochs):
+    for e_idx in range(epochs):
+        train_loader.sampler.set_epoch(e_idx)
+        tqdm.write(f"GPU {gpu_id}, EPOCH {e_idx}, N BATCHES {len(train_loader)} BATCH SIZE {train_loader.batch_size}")
         for b_idx, batch in tqdm(enumerate(train_loader), total=len(train_loader), desc="Train"):
-            xb = batch['video'].cuda()
-            yb = batch['class'].cuda()
+            xb = batch['video'].to(gpu_id)
+            yb = batch['class'].to(gpu_id)
 
             # Mixed precision forward pass
             with torch.autocast(device_type='cuda', dtype=torch.float16):
@@ -40,13 +44,14 @@ def train(model:nn.Module, writer:SummaryWriter, train_loader:DataLoader, val_lo
 
             # Stats and eval
             running_loss += loss
-            writer.add_scalar('Train/Loss', loss, global_step)
-            writer.add_scalar('Train/Lr', optimizer.param_groups[0]['lr'], global_step) # https://discuss.pytorch.org/t/get-current-lr-of-optimizer-with-adaptive-lr/24851/5?u=felipe_marra
+            if gpu_id == 0:
+                writer.add_scalar('Train/Loss', loss, global_step)
+                writer.add_scalar('Train/Lr', optimizer.param_groups[0]['lr'], global_step) # https://discuss.pytorch.org/t/get-current-lr-of-optimizer-with-adaptive-lr/24851/5
 
             steps_till_eval = (b_idx+1) % eval_every
             if steps_till_eval == 0 or b_idx+1 == len(train_loader):
                 mean_train_loss = running_loss/(eval_every - steps_till_eval)
-                mean_eval_loss = eval(model, val_loader, writer, global_step)
+                mean_eval_loss = eval(model, val_loader, writer, global_step, gpu_id)
 
                 tqdm.write(f"\n iter {b_idx+1} | train loss: {mean_train_loss:.4f}, eval loss: {mean_eval_loss:.4f} \n")
 
@@ -56,23 +61,24 @@ def train(model:nn.Module, writer:SummaryWriter, train_loader:DataLoader, val_lo
 
         #%%
         # Save model
-        checkpoint = {
-            "model": model.state_dict(),
-            "optimizer": criterion.state_dict(),
-            "scaler": scaler.state_dict()
-        }
+        if gpu_id == 0:
+            checkpoint = {
+                "model": model.module.state_dict(),
+                "optimizer": criterion.state_dict(),
+                "scaler": scaler.state_dict()
+            }
 
-        torch.save(checkpoint, f"./model_t_{mean_train_loss:.4f}_e_{mean_eval_loss:.4f}.pth")
+            torch.save(checkpoint, f"./model_t_{mean_train_loss:.4f}_e_{mean_eval_loss:.4f}.pth")
 
 @torch.no_grad()
-def eval(model:nn.Module, loader:DataLoader, writer:SummaryWriter, global_step:int):
+def eval(model:nn.Module, loader:DataLoader, writer:SummaryWriter, global_step:int, gpu_id:int):
     model = model.eval()
-    criterion = nn.CrossEntropyLoss().cuda()
+    criterion = nn.CrossEntropyLoss()
 
     losses = torch.zeros(len(loader))
     for b_idx, batch in tqdm(enumerate(loader), total=len(loader), desc="Eval"):
-        xb = batch['video'].cuda()
-        yb = batch['class'].cuda()
+        xb = batch['video'].to(gpu_id)
+        yb = batch['class'].to(gpu_id)
 
         # Mixed precision forward pass
         with torch.autocast(device_type='cuda', dtype=torch.float16):
@@ -85,26 +91,27 @@ def eval(model:nn.Module, loader:DataLoader, writer:SummaryWriter, global_step:i
     model = model.train()
 
     loss = losses.mean().item()
-    writer.add_scalar('Eval/Loss', loss, global_step)
+
+    if gpu_id == 0:
+        writer.add_scalar('Eval/Loss', loss, global_step)
 
     return loss
 
 @torch.no_grad()
-def test(model:nn.Module, loader:DataLoader, writer:SummaryWriter, checkpoint=None):
-    criterion = nn.CrossEntropyLoss().cuda()
+def test(model:nn.Module, loader:DataLoader, writer:SummaryWriter, gpu_id:int, checkpoint=None):
+    criterion = nn.CrossEntropyLoss()
 
     if checkpoint:
         model.load_state_dict(checkpoint["model"])
         criterion.load_state_dict(checkpoint["optimizer"])
 
     model = model.eval()
-    model = model.cuda()
 
     losses = torch.zeros(len(loader))
     acc_sum = 0
     for b_idx, batch in tqdm(enumerate(loader), total=len(loader), desc="Test"):
-        xb = batch['video'].cuda()
-        yb = batch['class'].cuda()
+        xb = batch['video'].to(gpu_id)
+        yb = batch['class'].to(gpu_id)
 
         # Mixed precision forward pass
         with torch.autocast(device_type='cuda', dtype=torch.float16):
@@ -125,7 +132,8 @@ def test(model:nn.Module, loader:DataLoader, writer:SummaryWriter, checkpoint=No
     acc = acc_sum/n_examples
     loss = losses.mean()
 
-    writer.add_scalar('Test/Acc', acc)
-    writer.add_scalar('Test/Loss', loss)
+    if gpu_id == 0:
+        writer.add_scalar('Test/Acc', acc)
+        writer.add_scalar('Test/Loss', loss)
 
     return loss, acc
